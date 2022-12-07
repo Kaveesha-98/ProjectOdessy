70f69a4087423cd97e79a10ea9d132bc993d411c
Chamindu Sauranga <chamindu1567@gmail.com>
2022-12-07 11:09:11 +0530

    Added the opCode input

--- Alu/src/main/scala/Alu.scala
+++ Alu/src/main/scala/Alu.scala
@@ -1,10 +1,13 @@
 import chisel3._
 import chisel3.util._
 
+import Constants._
+
 class DecodeIssuePort extends Bundle {
   val valid       = UInt(1.W)
   val PC          = UInt(64.W)
   val instruction = UInt(32.W)
+  val opCode      = UInt(7.W)
   val rs1         = UInt(64.W)
   val rs2         = UInt(64.W)
   val immediate   = UInt(64.W)
@@ -43,11 +46,33 @@ class Alu extends Module{
   val immReg   = RegInit(0.U(64.W))
   val rs1Reg   = RegInit(0.U(64.W))
   val rs2Reg   = RegInit(0.U(64.W))
+  val opCodeReg= RegInit(0.U(7.W))
 
-  val insType = WireDefault(0.U)
+  pcReg     := io.decodeIssuePort.PC
+  insReg    := io.decodeIssuePort.instruction
+  immReg    := io.decodeIssuePort.imm
+  rs1Reg    :- io.decodeIssuePort.rs1
+  rs2Reg    :- io.decodeIssuePort.rs2
+  opCodeReg :- io.decodeIssuePort.opCode
 
-  pcReg  := io.decodeIssuePort.PC
-  insReg := io.decodeIssuePort.instruction
+  switch (opCode) {
+    is(lui.U)    { insType := utype.U }
+    is(auipc.U)  { insType := utype.U }
+    is(jump.U)   { insType := jtype.U }
+    is(jumpr.U)  { insType := itype.U }
+    is(cjump.U)  { insType := btype.U }
+    is(load.U)   { insType := itype.U }
+    is(store.U)  { insType := stype.U }
+    is(iops.U)   { insType := itype.U }
+    is(iops32.U) { insType := itype.U }
+    is(rops.U)   { insType := rtype.U }
+    is(rops32.U) { insType := rtype.U }
+    is(system.U) { insType := itype.U }
+    is(fence.U)  { insType := ntype.U }
+    is(amos.U)   { insType := rtype.U }
+  }
+
+  
 
 }
 
