26b9b83add9740db6b7a6389ff63a7666342d027
Chamindu Sauranga <chamindu1567@gmail.com>
2022-12-08 13:37:53 +0530

    Added the execution flow for some instructions

--- Alu/src/main/scala/Alu.scala
+++ Alu/src/main/scala/Alu.scala
@@ -58,24 +58,73 @@ class Alu extends Module{
   opCodeReg :- io.decodeIssuePort.opCode
   funct3Reg :- io.decodeIssuePort.instruction(14,12)
 
+
+val aluResultReg = RegInit(0.U(64.W))
+val nextInstPtrReg = RegInit(0.U(64.W))
+val branchResultValidReg = RegInit(0.U(1.W))
+val branchResultTargetReg = RegInit(0.U(64.W))
+
+io.aluIssuePort.aluResult := aluResultReg
+io.aluIssuePort.nextInstrPtr := nextInstPtrReg
+io.branchResult.valid := branchResultValidReg
+io.branchResult.target := branchResultTargetReg
   
 
 
   switch (opCode) {
-    is(lui.U)    { insType := utype.U }
-    is(auipc.U)  { insType := utype.U }
-    is(jump.U)   { insType := jtype.U }
-    is(jumpr.U)  { insType := itype.U }
-    is(cjump.U)  { insType := btype.U }
-    is(load.U)   { insType := itype.U }
-    is(store.U)  { insType := stype.U }
-    is(iops.U)   { insType := itype.U }
-    is(iops32.U) { insType := itype.U }
-    is(rops.U)   { insType := rtype.U }
-    is(rops32.U) { insType := rtype.U }
-    is(system.U) { insType := itype.U }
-    is(fence.U)  { insType := ntype.U }
-    is(amos.U)   { insType := rtype.U }
+    is(lui.U)    { 
+      aluResultReg            := immReg
+      nextInstPtrReg          := pcReg + 4.U
+      branchResultValidReg    := 0.U     
+      }
+
+    is(auipc.U)  { 
+      aluResultReg            := pcReg + immReg
+      nextInstPtrReg          := pcReg + 4.U
+      branchResultValidReg    := 0.U     
+     }
+    is(jump.U)   { 
+      aluResultReg            := pcReg + 4.U
+      nextInstPtrReg          := pcReg + immReg
+      branchResultValidReg    := 1.U
+      branchResultTargetReg   := pcReg + immReg          
+     }
+    is(jumpr.U)  { 
+      aluResultReg            := pcReg + 4.U
+      nextInstPtrReg          := rs1Reg + immReg
+      branchResultValidReg    := 1.U
+      branchResultTargetReg   := rs1Reg + immReg               
+     }
+    is(cjump.U)  { 
+      
+     }
+    is(load.U)   { 
+      
+     }
+    is(store.U)  { 
+      
+     }
+    is(iops.U)   { 
+      
+     }
+    is(iops32.U) { 
+      
+     }
+    is(rops.U)   { 
+      
+     }
+    is(rops32.U) { 
+      
+     }
+    is(system.U) { 
+      
+     }
+    is(fence.U)  { 
+      
+     }
+    is(amos.U)   { 
+      
+     }
   }
 
   
