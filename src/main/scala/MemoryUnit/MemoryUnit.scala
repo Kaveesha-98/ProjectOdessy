347dd51b1013922fe122166ac7bb3ea0d3cae163
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-12-24 15:21:15 +0530

    added a way to initialize tilelink d channel

--- src/main/scala/MemoryUnit/MemoryUnit.scala
+++ src/main/scala/MemoryUnit/MemoryUnit.scala
@@ -97,6 +97,40 @@ class channel_d(
     val error   = Input(UInt(1.W))
     val valid   = Input(UInt(1.W))
     val ready   = Output(UInt(1.W))
+
+    class sourceReg extends Bundle {
+        val opcode      = Output(UInt(3.W))
+        val param       = Output(UInt(3.W))
+        val size        = Output(UInt(z.W))
+        val source      = Output(UInt(o.W))
+        val sink        = Input(UInt(i.W))
+        val data        = Output(UInt((8*w).W))
+        val error       = Input(UInt(1.W))
+        val valid       = Output(UInt(1.W))
+    }
+
+    def init() =
+        (new sourceReg).Lit(
+            _.opcode -> 0.U,
+            _.param -> 0.U,
+            _.size -> 0.U,
+            _.source -> 0.U,
+            _.sink -> 0.U,
+            _.data -> 0.U,
+            _.error -> 0.U,
+            _.valid -> 0.U
+        )
+
+    def :=(sink_source: sourceReg):Unit = {
+        this.opcode      := sink_source.opcode
+        this.param       := sink_source.param
+        this.size        := sink_source.size
+        this.source      := sink_source.source
+        this.sink        := sink_source.sink
+        this.data        := sink_source.data
+        this.error       := sink_source.error
+        this.valid       := sink_source.valid
+    } 
 }
 
 class MemoryUnit extends Module {
