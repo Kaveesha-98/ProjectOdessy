293b0b644631d6817f58d869513fb5cd00e51bac
daksith <daksith@gmail.com>
2023-01-14 16:59:28 +0530

    Static branch prediction,emitverilog successfull

--- FetchUnit/src/main/scala/FetchUnit.scala
+++ FetchUnit/src/main/scala/FetchUnit.scala
@@ -2,22 +2,21 @@
  Fetch Unit for in-order with speculation
  */
 
+import Chisel.Cat
 import chisel3._
 
-class branch_detector extends Module {
+class BHT extends Module {
   val io = IO(new Bundle {
-    val instr  = Input(UInt(32.W))
-    val is_branch = Output(UInt(1.W))
+    val execport = new Execport()
+    val curr_pc = Input(UInt(64.W))
+    val next_pc = Output(UInt(64.W))
   })
 
-  val op1 = io.instr(6,5)
-  val op2 = io.instr(4,2)
-
-  val flag1 = op1 === "b11".U
-  val flag2 = op2 === "b001".U || op2 === "b011".U || op2 === "b000".U
-
-  io.is_branch :=  flag1 & flag2
-
+  when (!io.execport.predicted) {
+    io.next_pc := io.execport.branch_address
+  } .otherwise {
+    io.next_pc := io.curr_pc + 4.U
+  }
 }
 
 class FetchUnit(val pc_reset_val: Int, val fifo_size: Int) extends Module {
@@ -33,43 +32,37 @@ class FetchUnit(val pc_reset_val: Int, val fifo_size: Int) extends Module {
   val IR = RegInit(0.U(32.W))
   val internal_stall = RegInit(0.U(1.W))
   val IR_valid = RegInit(0.U(1.W))
-  val PC_valid = RegInit(1.U(1.W))
-  val insport_pc = RegInit(0.U(64.W))
+  val insport_pc = RegInit(0.U(128.W))
 
-  // initialize branch detector and fifo buffer
-  val branch_detector  = Module(new branch_detector)
-  val PC_fifo  = Module(new RegFifo(UInt(64.W), fifo_size))
+  // initialize BHT and fifo buffer
+  val bht = Module(new BHT)
+  bht.io.execport <> io.execport
+  bht.io.curr_pc := PC
+  val PC_fifo  = Module(new RegFifo(UInt(128.W), fifo_size))
 
   //connect PC_fifo
-  PC_fifo.io.enq.bits := PC
+  PC_fifo.io.enq.bits := Cat(PC,bht.io.next_pc)
   PC_fifo.io.enq.valid := io.reqport.valid & io.reqport.ready
   PC_fifo.io.deq.ready := io.resport.valid & io.issueport.ready
 
   when (io.issueport.ready === 1.U) {
     insport_pc := PC_fifo.io.deq.bits
   }
-  io.issueport.PC := insport_pc
+  io.issueport.PC := insport_pc(127,64)
+  io.issueport.prediction := insport_pc(63,0)
 
   //PC update logic
-  when (internal_stall===1.U & io.execport.valid===1.U){
-    PC := io.execport.branch_address
-  } .elsewhen(io.reqport.ready === 1.U & io.reqport.valid === 1.U){
-    PC := PC + 4.U
+  when (internal_stall===0.U & io.reqport.valid & io.reqport.ready){
+    PC := bht.io.next_pc
+  }.elsewhen(internal_stall===0.U & (!io.execport.predicted) & (io.execport.valid)){  //special case where pc buffer is full and a mispred occurs
+    PC := bht.io.next_pc
   }
   io.reqport.bits := PC
 
-  //PC valid bit logic
-  when (branch_detector.io.is_branch === 1.U & PC_valid === 1.U){
-    PC_valid := 0.U
-  } .elsewhen(io.execport.valid === 1.U){
-    PC_valid := 1.U
-  }
-
-
   //ready valid signal logic
-  io.reqport.valid := ~internal_stall & PC_fifo.io.enq.ready
+  io.reqport.valid := internal_stall===0.U & PC_fifo.io.enq.ready
   io.resport.ready := io.issueport.ready & PC_fifo.io.deq.valid
-  io.issueport.valid := IR_valid & (~internal_stall) & (io.issueport.ready)
+  io.issueport.valid := IR_valid & (internal_stall===0.U) & (io.issueport.ready)
 
 
   //IR update logic
@@ -77,7 +70,6 @@ class FetchUnit(val pc_reset_val: Int, val fifo_size: Int) extends Module {
     IR := io.resport.bits
   }
   io.issueport.ins := IR
-  branch_detector.io.instr := IR
   when(IR_valid === 0.U) {
     IR_valid := (io.resport.ready === 1.U & io.resport.valid === 1.U & internal_stall === 0.U & io.issueport.ready === 1.U).asUInt
   }.otherwise {
@@ -86,8 +78,8 @@ class FetchUnit(val pc_reset_val: Int, val fifo_size: Int) extends Module {
 
   //stall logic
   when (internal_stall === 0.U){
-    internal_stall := branch_detector.io.is_branch & IR_valid
-  } .elsewhen (PC_fifo.io.deq.valid === 0.U & PC_valid === 1.U){
+    internal_stall := io.execport.predicted
+  } .elsewhen (PC_fifo.io.deq.valid === 0.U){
     internal_stall := 0.U
   }
   printf(p"$io\n")
