a70912fe551fce6712140d1fbae69bc5ba964215
daksith <daksith@gmail.com>
2023-01-15 14:34:45 +0530

    Initial functional verification complete

--- FetchUnit/src/test/scala/FetchUnitTester.scala
+++ FetchUnit/src/test/scala/FetchUnitTester.scala
@@ -4,43 +4,67 @@ import org.scalatest.freespec.AnyFreeSpec
 
 class FetchUnitTester extends AnyFreeSpec with ChiselScalatestTester {
   "Testing FetchUnit" in {
-    test(new FetchUnit(0,16)) { c =>
-      c.io.reqport_ready.poke(1.U)
-      c.io.resport_valid.poke(0.U)
-      c.io.pipelinestalled.poke(0.U)
-      c.io.target_input.poke(0.U)
-      c.io.target_valid.poke(0.U)
-      c.io.resport_instr.poke(0.U)
+    test(new FetchUnit(0,32)) { c =>
+      c.io.reqport.ready.poke(1.U)
+      c.io.resport.valid.poke(0.U)
+      c.io.issueport.ready.poke(1.U)
+      c.io.execport.valid.poke(0.U)
+      c.io.execport.branch_taken.poke(0.U)
+      c.io.execport.predicted.poke(1.U)
+      c.io.execport.PC.poke(1.U)
+      c.io.execport.branch_address.poke(1.U)
+      c.io.execport.is_branch.poke(1.U)
+      c.io.resport.bits.poke(0.U)
       c.clock.step(1)
-      //for loop
-      for (i <- 1 until 30) {
-        c.io.reqport_ready.poke(1.U)
-        c.io.resport_valid.poke(0.U)
-        c.io.pipelinestalled.poke(0.U)
-        c.io.target_input.poke(0.U)
-        c.io.target_valid.poke(0.U)
-        c.io.resport_instr.poke(i.U)
+      c.clock.step(1)
+
+      for (i <- 0 until 10) {
+        c.clock.step(1)
+      }
+      for (i <- 0 until 10){
+        c.io.resport.valid.poke(1.U)
+        c.io.resport.bits.poke(i.U)
+        c.clock.step(1)
+      }
+      //pipeline stall
+      for (i <- 10 until 20) {
+        c.io.resport.valid.poke(1.U)
+        c.io.resport.bits.poke(10.U)
+        c.io.issueport.ready.poke(0.U)
+        c.clock.step(1)
+      }
+      //resume
+      for (i <- 10 until 20) {
+        c.io.resport.valid.poke(1.U)
+        c.io.resport.bits.poke(i.U)
+        c.io.issueport.ready.poke(1.U)
         c.clock.step(1)
       }
 
-      for (i <- 1 until 29) {
-        c.io.reqport_ready.poke(0.U)
-        c.io.resport_valid.poke(1.U)
-        c.io.pipelinestalled.poke(0.U)
-        c.io.target_input.poke(0.U)
-        c.io.target_valid.poke(0.U)
-        c.io.resport_instr.poke(i.U)
+      c.io.execport.valid.poke(1.U)
+      c.io.execport.branch_taken.poke(0.U)
+      c.io.execport.predicted.poke(0.U)
+      c.io.execport.PC.poke(1.U)
+      c.io.execport.branch_address.poke(200.U)
+      c.io.execport.is_branch.poke(1.U)
+      c.io.resport.bits.poke(0.U)
+      c.clock.step(1)
+
+      for (i <- 20 until 44) {
+        c.io.execport.valid.poke(0.U)
+        c.io.resport.valid.poke(1.U)
+        c.io.resport.bits.poke(i.U)
+        c.io.issueport.ready.poke(1.U)
         c.clock.step(1)
       }
-      for (i <- 1 until 29) {
-        c.io.reqport_ready.poke(1.U)
-        c.io.resport_valid.poke(1.U)
-        c.io.pipelinestalled.poke(0.U)
-        c.io.target_input.poke(0.U)
-        c.io.target_valid.poke(0.U)
-        c.io.resport_instr.poke(i.U)
+
+      for (i <- 200 until 210) {
+        c.io.resport.valid.poke(1.U)
+        c.io.resport.bits.poke(i.U)
+        c.io.issueport.ready.poke(1.U)
         c.clock.step(1)
       }
+
     }
   }
 }
