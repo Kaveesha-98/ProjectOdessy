5ac40a6f4ca8bd9fbeb96d7e9b47c6bd6233c3b7
daksith <daksith@gmail.com>
2023-01-15 14:56:15 +0530

    Tested case where fifo buffer is full and a misprediction occurs

--- FetchUnit/src/test/scala/FetchUnitTester.scala
+++ FetchUnit/src/test/scala/FetchUnitTester.scala
@@ -18,29 +18,9 @@ class FetchUnitTester extends AnyFreeSpec with ChiselScalatestTester {
       c.clock.step(1)
       c.clock.step(1)
 
-      for (i <- 0 until 10) {
+      for (i <- 0 until 35) {
         c.clock.step(1)
       }
-      for (i <- 0 until 10){
-        c.io.resport.valid.poke(1.U)
-        c.io.resport.bits.poke(i.U)
-        c.clock.step(1)
-      }
-      //pipeline stall
-      for (i <- 10 until 20) {
-        c.io.resport.valid.poke(1.U)
-        c.io.resport.bits.poke(10.U)
-        c.io.issueport.ready.poke(0.U)
-        c.clock.step(1)
-      }
-      //resume
-      for (i <- 10 until 20) {
-        c.io.resport.valid.poke(1.U)
-        c.io.resport.bits.poke(i.U)
-        c.io.issueport.ready.poke(1.U)
-        c.clock.step(1)
-      }
-
       c.io.execport.valid.poke(1.U)
       c.io.execport.branch_taken.poke(0.U)
       c.io.execport.predicted.poke(0.U)
@@ -50,20 +30,72 @@ class FetchUnitTester extends AnyFreeSpec with ChiselScalatestTester {
       c.io.resport.bits.poke(0.U)
       c.clock.step(1)
 
-      for (i <- 20 until 44) {
-        c.io.execport.valid.poke(0.U)
+      for (i <- 0 until 32) {
         c.io.resport.valid.poke(1.U)
         c.io.resport.bits.poke(i.U)
         c.io.issueport.ready.poke(1.U)
         c.clock.step(1)
       }
 
-      for (i <- 200 until 210) {
+      c.io.reqport.ready.poke(1.U)
+      c.io.resport.valid.poke(0.U)
+      c.io.issueport.ready.poke(1.U)
+      c.io.execport.valid.poke(0.U)
+      c.io.execport.branch_taken.poke(0.U)
+      c.io.execport.predicted.poke(1.U)
+      c.io.execport.PC.poke(1.U)
+      c.io.execport.branch_address.poke(1.U)
+      c.io.execport.is_branch.poke(1.U)
+      c.io.resport.bits.poke(0.U)
+      c.clock.step(1)
+      c.clock.step(1)
+      c.clock.step(1)
+
+      for (i <- 200 until 220) {
         c.io.resport.valid.poke(1.U)
         c.io.resport.bits.poke(i.U)
         c.io.issueport.ready.poke(1.U)
         c.clock.step(1)
       }
+
+      //pipeline stall
+//      for (i <- 10 until 20) {
+//        c.io.resport.valid.poke(1.U)
+//        c.io.resport.bits.poke(10.U)
+//        c.io.issueport.ready.poke(0.U)
+//        c.clock.step(1)
+//      }
+//      //resume
+//      for (i <- 10 until 20) {
+//        c.io.resport.valid.poke(1.U)
+//        c.io.resport.bits.poke(i.U)
+//        c.io.issueport.ready.poke(1.U)
+//        c.clock.step(1)
+//      }
+//
+//      c.io.execport.valid.poke(1.U)
+//      c.io.execport.branch_taken.poke(0.U)
+//      c.io.execport.predicted.poke(0.U)
+//      c.io.execport.PC.poke(1.U)
+//      c.io.execport.branch_address.poke(200.U)
+//      c.io.execport.is_branch.poke(1.U)
+//      c.io.resport.bits.poke(0.U)
+//      c.clock.step(1)
+//
+//      for (i <- 20 until 44) {
+//        c.io.execport.valid.poke(0.U)
+//        c.io.resport.valid.poke(1.U)
+//        c.io.resport.bits.poke(i.U)
+//        c.io.issueport.ready.poke(1.U)
+//        c.clock.step(1)
+//      }
+//
+//      for (i <- 200 until 210) {
+//        c.io.resport.valid.poke(1.U)
+//        c.io.resport.bits.poke(i.U)
+//        c.io.issueport.ready.poke(1.U)
+//        c.clock.step(1)
+//      }
 
     }
   }
