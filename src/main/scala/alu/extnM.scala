f3c932091765af62ad0fde57e86b95b3f920cdd4
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2023-06-02 18:02:34 +0530

    single cycle M

--- src/main/scala/alu/extnM.scala
+++ src/main/scala/alu/extnM.scala
@@ -26,25 +26,28 @@ class mExten extends Module {
     result := {
       val result64 = VecInit.tabulate(8)(i => i match {
         case 0 => (input.bits.src1.asSInt * input.bits.src2.asSInt).asUInt // mul
-        case 1 => 0.U // mulh
-        case 2 => 0.U // mulhsu
+        case 1 => (input.bits.src1.asSInt * input.bits.src2.asSInt)(127, 64).asUInt // mulh
+        case 2 => (Cat(input.bits.src1(63), input.bits.src1).asSInt * Cat(0.U(1.W) ,input.bits.src2).asSInt)(127, 64).asUInt // mulhsu
         case 3 => (input.bits.src1 * input.bits.src2)(127, 64) // mulhu
-        case 4 => 0.U // div
-        case 5 => 0.U // divu
-        case 6 => 0.U // rem
-        case 7 => 0.U // remu
+        case 4 => Mux(input.bits.src2 === 0.U, ~(0.U(64.W)), (input.bits.src1.asSInt / input.bits.src2.asSInt).asUInt) // div
+        case 5 => Mux(input.bits.src2 === 0.U, ~(0.U(64.W)), (input.bits.src1 / input.bits.src2))// divu
+        case 6 => (input.bits.src1.asSInt - Mux(input.bits.src2 === 0.U, (-1).S(64.W), (input.bits.src1.asSInt / input.bits.src2.asSInt))*(input.bits.src2.asSInt) ).asUInt  // rem
+        case 7 => (input.bits.src1 - Mux(input.bits.src2 === 0.U, ~(0.U(64.W)), (input.bits.src1 / input.bits.src2))*(input.bits.src2) ) // remu
       })(input.bits.instruction(14, 12))
 
-      val result32 = VecInit.tabulate(8)(i => i match {
-        case 0 => 0.U // mulw
+      val src1W = input.bits.src1(31,0)
+      val src2W = input.bits.src2(31,0)
+      val result32W = VecInit.tabulate(8)(i => i match {
+        case 0 => (src1W.asSInt * src2W.asSInt)(31,0).asUInt // mulw
         case 1 => 0.U // not defined
         case 2 => 0.U // not defined
         case 3 => 0.U // not defined
-        case 4 => 0.U // divw
-        case 5 => 0.U // divuw
-        case 6 => 0.U // remw
-        case 7 => 0.U // remuw
+        case 4 => Mux(src2W === 0.U, ~(0.U(32.W)), (src1W.asSInt / src2W.asSInt).asUInt)(31,0)  // divw
+        case 5 => Mux(src2W === 0.U, ~(0.U(64.W)), (src1W / src2W))(31,0) // divuw
+        case 6 => ((src1W.asSInt - Mux(src2W === 0.U, (-1).S(32.W), (src1W.asSInt / src2W.asSInt))*(src2W.asSInt) ).asUInt)(31,0) // remw
+        case 7 => (src1W - Mux(src2W === 0.U, ~(0.U(32.W)), (src1W / src2W))*(src2W) )(31,0)  // remuw
       })(input.bits.instruction(14, 12))
+      val result32 = Cat(Fill(32, result32W(31)), result32W)
 
       Mux(input.bits.instruction(3).asBool, result32, result64)
     }
